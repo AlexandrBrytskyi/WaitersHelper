e277b11a0bdce6510703faad2a5e6e5dbf58ebff
Brytskyi <brytskyi@mail.ua>
2016-02-28 19:12:55 +0200

    test rmi

---
+++ CommonTransfer/src/main/java/transferFiles/to/Loginable.java
@@ -0,0 +1,10 @@
+package transferFiles.to;
+
+
+/*interface has only one method, which must be realised by UI, such that when we create UI, we send
+* transferFiles.to the validator UI odject */
+
+public interface Loginable {
+
+    void sendUIToLoginedList();
+}
